e62bc5927ddc3504134d6c32107054a6d2fd13ea
chendan <1532758168@qq.com>
2020-04-21 12:25:42 +0800

    初始化项目

---
+++ src/main/java/com/github/myoa/MyOaApplication.java
@@ -0,0 +1,13 @@
+package com.github.myoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/myoa/MyOaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.myoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
